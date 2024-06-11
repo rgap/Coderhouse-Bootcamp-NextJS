aa04f6df211a8c31bb7191e6a4fba65bdf479388
rgap <r.guzmanap@gmail.com>
2024-06-11 16:54:25 -0500

    data cache bug

--- 56. data-cache-codevolution/src/app/products/page.tsx
+++ 56. data-cache-codevolution/src/app/products/page.tsx
@@ -6,9 +6,18 @@ type Product = {
 };
 
 export default async function ProductsPage() {
-  // Automatically caches the returned values
-  const response = await fetch("http://localhost:3001/products");
+  const detailsResponse = await fetch("http://localhost:3001/products/1");
+  const details = await detailsResponse.json();
+
+  const response = await fetch("http://localhost:3001/products", {
+    cache: "no-store", // Disable cache
+  });
   const products = await response.json();
+
+  // after a "no-store", it will not cache every other fetched data
+  // const detailsResponse = await fetch("http://localhost:3001/products/1");
+  // const details = await detailsResponse.json();
+
   return (
     <ul className="space-y-4 p-4">
       {products.map((product: Product) => (
@@ -16,6 +25,7 @@ export default async function ProductsPage() {
           <h2 className="text-xl font-semibold">{product.title}</h2>
           <p>{product.description}</p>
           <p className="text-lg font-medium">${product.price}</p>
+          <p>{details.price}</p>
         </li>
       ))}
     </ul>
